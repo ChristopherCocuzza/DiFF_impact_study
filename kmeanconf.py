d96d6df9570a0c4661fee1b3fd89b78d630ae7fd
Christopher Cocuzza <ccocuzza@farm180246.jlab.org>
2023-07-03 10:34:38 -0400

    update

---
+++ kmeanconf.py
@@ -0,0 +1,100 @@
+
+nc={}
+hooks={}
+
+nc[1] = 1
+hooks[1] = None
+nc[2] = 1
+hooks[2] = None
+nc[3] = 1
+hooks[3] = None
+nc[4] = 1
+hooks[4] = None
+nc[5] = 1
+hooks[5] = None
+nc[6] = 1
+hooks[6] = None
+nc[7] = 1
+hooks[7] = None
+nc[8] = 1
+hooks[8] = None
+nc[9] = 1
+hooks[9] = None
+nc[10] = 1
+hooks[10] = None
+nc[11] = 1
+hooks[11] = None
+nc[12] = 1
+hooks[12] = None
+nc[13] = 1
+hooks[13] = None
+nc[14] = 1
+hooks[14] = None
+nc[15] = 1
+hooks[15] = None
+nc[16] = 1
+hooks[16] = None
+nc[17] = 1
+hooks[17] = None
+nc[18] = 1
+hooks[18] = None
+nc[19] = 1
+hooks[19] = None
+nc[20] = 1
+hooks[20] = None
+nc[21] = 1
+hooks[21] = None
+#nc[21] = 3
+#hooks[21] = _hook_strange
+nc[22] = 1
+hooks[22] = None
+nc[23] = 1
+hooks[23] = None
+nc[24] = 1
+hooks[24] = None
+nc[25] = 1
+hooks[25] = None
+nc[26] = 1
+hooks[26] = None
+nc[27] = 1
+hooks[27] = None
+nc[28] = 1
+hooks[28] = None
+nc[29] = 1
+hooks[29] = None
+nc[30] = 1
+hooks[30] = None
+nc[31] = 1
+hooks[31] = None
+nc[32] = 1
+hooks[32] = None
+nc[33] = 1
+hooks[33] = None
+nc[34] = 1
+hooks[34] = None
+nc[35] = 1
+hooks[35] = None
+nc[36] = 1
+hooks[36] = None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
